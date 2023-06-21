98761b51ba8c6cb867f1800579467362bdbfa890
Brad Mering <brad@convertiv.com>
2023-06-20 22:03:12 -0600

    Tailwind and Bootstrap test cases

---
+++ bootstrap.js
@@ -0,0 +1,21 @@
+import Handoff from "handoff-app";
+
+// To use the hook, uncomment this
+(async function () {
+  try {
+    const handoff = new Handoff.default({
+      title: "Handoff Bootstrap",
+      integration: {
+        name: "bootstrap",
+        version: "5.3"
+      }
+    });
+    await handoff.fetch();
+    await handoff.build();
+    
+    process.exit(0);
+  } catch (e) {
+    console.log(e);
+    process.exit(0);
+  }
+})();
